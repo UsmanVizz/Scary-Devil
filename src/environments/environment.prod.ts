51969bb49223212dd6982359d1087a399f4b81fc
Usman <usmanbinsaad@vizzweb.com>
2024-04-18 18:08:21 +0500

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@
 
 
 // };
-export const environment = {
-  production: false,
-  baseUrl: 'https://backend.tzugai.com/api/',
-};
+// export const environment = {
+//   production: false,
+//   baseUrl: 'https://backend.tzugai.com/api/',
+// };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,16 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//   production: false,
-//   baseUrl: 'http://localhost:3000/api/',
-// };
-
-
 export const environment = {
   production: false,
-  baseUrl: 'https://backend.tzugai.com/api/',
+  baseUrl: 'http://localhost:3000/api/',
 };
+
+
+// export const environment = {
+//   production: false,
+//   baseUrl: 'https://backend.tzugai.com/api/',
+// };
 
 /*
  * For easier debugging in development mode, you can import the following file
